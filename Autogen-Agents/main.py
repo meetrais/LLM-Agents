646ffd34e6e8111c4b7902b404b0f798017eedc7
Rais Kazi <meetrais@gmail.com>
2024-04-30 05:50:17 -0500

    Added web ui using Gradio.

--- Autogen-Agents/main.py
+++ Autogen-Agents/main.py
@@ -1,53 +1,25 @@
-import autogen
-import os
+import university as UN
+import gradio as gr
 
-llm_config = {
-    "config_list": [{"model": "gpt-3.5-turbo", "api_key": os.environ["OPENAI_API_KEY"]}],
-}
+def run_generation(question):
+    result = UN.get_answer(question)
+    result = result.chat_history
+    student = "Student: " + result[0]["content"] + "<br /><br />"
+    professor = result[1]["name"].replace("_"," ") + ": " + result[1]["content"]
+    
+    final_result="<html><div style='color:black;font-weight:bold;font-size: 1.25em;'>{}</div><div style='color:#000080;font-weight:bold;font-size: 1.25em;'>{}</div></html>".format(student,professor)
+    return final_result
+    
+if __name__ == "__main__":
+    # Gradio UI setup
+    with gr.Blocks() as demo:
+        with gr.Row():
+            with gr.Column(scale=4):
+                user_text = gr.Textbox(placeholder="Write your question here", label="Student Question")
+                model_output = gr.HTML(label="LLM Agents Output", show_label=True)
+                #button_submit = gr.Button(value="Submit")
 
-# Define Agents
+        user_text.submit(run_generation, [user_text], model_output)
+        #button_submit.click(run_generation, [user_text], model_output)
 
-user_proxy = autogen.UserProxyAgent(
-    name="University_Coordinator",
-    system_message="""A human user. select most appropriate assistant agent amongst Biology_Professor, Mathematics_Professor, Electronics_Professor
-        and Chemistry_Professor to get answer on question asked by user.""",
-    human_input_mode="TERMINATE",
-)
-
-professor_boilogy = autogen.AssistantAgent(
-    name="Biology_Professor",
-    llm_config=llm_config,
-    system_message="""Professor of Biology. YAs a professor of Biology answer question of student only if its related to Biology subject. 
-        If question is not related to Biology then only say Sorry, this is not my subject.""",
-)
-
-professor_mathematics = autogen.AssistantAgent(
-    name="Mathematics_Professor",
-    llm_config=llm_config,
-    system_message="""Professor of Mathematics. As a professor of Mathematics answer question of student only if its related to Mathematics subject. 
-        If question is not related to Mathematics then only say Sorry, this is not my subject.""",
-)
-
-professor_chemistry = autogen.AssistantAgent(
-    name="Chemistry_Professor",
-    system_message="""Professor of Mathematics. As a professor of Chemistry answer question of student only if its related to Chemistry subject.  
-        If question is not related to Chemistry then only say Sorry, this is not my subject.""",
-    llm_config=llm_config,
-)
-
-professor_electronics = autogen.AssistantAgent(
-    name="Electronics_Professor",
-    system_message="""Professor of Electronics. As a professor of Electronics answer question of student only if its related to Electronics subject.  
-        If question is not related to Electronics then only say Sorry, this is not my subject.""",
-    llm_config=llm_config,
-)
-
-group_chat = autogen.GroupChat(
-    agents=[user_proxy, professor_boilogy, professor_mathematics, professor_chemistry, professor_electronics], messages=[], max_round=2
-)
-manager = autogen.GroupChatManager(groupchat=group_chat, llm_config=llm_config)
-
-user_proxy.initiate_chat(
-    manager,
-    message="""What is the chemical formula for water?""",
-)
+        demo.queue(max_size=32).launch(server_port=8082)
